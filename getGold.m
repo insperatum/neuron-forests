b246f99a642eb3f80a29a25bf998107d584efabb
luke <insperatum@gmail.com>
2014-09-27 12:50:03 +0100

    Generate labels either as boundary/not-boundary or as affinity graph

--- getGold.m
+++ getGold.m
@@ -1,6 +1,10 @@
-function [gold] = getGold(segmented)
-    seg1 = segmented(1:end-1, 1:end-1, 1:end-1);
-    gold(:,:,:,1) = (seg1 ~= 0) & (seg1 == segmented(2:end, 1:end-1, 1:end-1));
-    gold(:,:,:,2) = (seg1 ~= 0) & (seg1 == segmented(1:end-1, 2:end, 1:end-1));
-    gold(:,:,:,3) = (seg1 ~= 0) & (seg1 == segmented(1:end-1, 1:end-1, 2:end));
+function [gold] = getGold(segmented, type)
+    if(nargin > 1 && strcmp(type, 'affinity'))
+        seg1 = segmented(1:end-1, 1:end-1, 1:end-1);
+        gold(:,:,:,1) = (seg1 ~= 0) & (seg1 == segmented(2:end, 1:end-1, 1:end-1));
+        gold(:,:,:,2) = (seg1 ~= 0) & (seg1 == segmented(1:end-1, 2:end, 1:end-1));
+        gold(:,:,:,3) = (seg1 ~= 0) & (seg1 == segmented(1:end-1, 1:end-1, 2:end));
+    else
+        gold = segmented(1:end-1, 1:end-1, 1:end-1) ~= 0;
+    end
 end
