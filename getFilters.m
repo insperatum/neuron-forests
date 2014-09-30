938d368f8dcf3abee446cbac1b3e4a84c8771dad
luke <insperatum@gmail.com>
2014-09-30 23:50:42 +0100

    Function for planar gaussian derivative features

--- getFilters.m
+++ getFilters.m
@@ -24,5 +24,30 @@ function [fs] = getFilters(sigmas)
         end
     end
     fs = fs(:);
+    
+    %todo:
+    %getPlaneDerivativeFilters(...);
+end
+                
+function [fs] = getPlaneDerivativeFilters(sigma1, sigma23)
+    r = max(sigma1, sigma23);
+    [Y, X, Z] = meshgrid(-2*r:2*r, -2*r:2*r, -2*r:2*r);
+
+    s2 = 1/sqrt(2); s3=1/sqrt(3);
+    directions = [0 0 1; 0 s2 -s2; 0 1 0; 0 s2 s2; ...
+                  s3 -s3 -s3; s2 -s2 0; s3 -s3 s3; ...
+                  s2 0 -s2; 1 0 0; s2 0 s2; ...
+                  s3 s3 -s3; s2 s2 0; s3 s3 s3 ];
+              
+    fs = cell(1, size(directions,1));
+    for i = 1:size(directions,1)
+        v = directions(i,:)';
+        V = [v null(v')];
+        D = diag([sigma1 sigma23*ones(1, length(v)-1)]);
+        C = V * D^2 * V';
+        gFil = reshape(mvnpdf([X(:) Y(:) Z(:)], [0 0 0], C), length(X), length(Y), length(Z));
+        dFil = cat(3, [-(v(1)+v(2)+v(3))/2 v(2)/2; v(1)/2 0], [v(3)/2 0; 0 0]);
+        fs{i} = convn(gFil, dFil);
+    end
 end
 
